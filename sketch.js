8ee4fb4b1455db9c38eec0489f7a5865b14e0555
MirrorMiru <80078855+MirrorMiru@users.noreply.github.com>
2021-05-10 16:35:52 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango
 var world,boy;
 
 //Declare launcherObject and launchForce variable here
-var launchForce = 100
+
 
 function preload(){
 	boy=loadImage("boy.png");
